c201e1e5cbdeb8080e543137a3617d30c5c142a6
Estela Carina Gonzalez Ruiz <estela.gonzalez@MacBook-Pro-de-Proactivanet.local>
2018-05-03 00:00:20 -0500

    thankspage

---
+++ src/app/components/TanksPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+class ThanksPage extends Component {
+  render() {
+    const logo = '../app/content/img/logo.png';
+    const rutaBase = '/'; /*Para prod debe ir / */ 
+    return (
+      <div>
+          Hola
+      </div>
+    );
+  }
+}
+
+export default ThanksPage;
